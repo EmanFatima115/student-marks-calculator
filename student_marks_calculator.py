3a4c5fb40d57dc95b6e945588f2b00a5a5c2f58f
EmanFatima115 <emanf5699@gmail.com>
2026-08-15 14:40:09 +0500

    Add student marks calculator

---
+++ student_marks_calculator.py
@@ -0,0 +1,11 @@
+name=input('enter your name:')
+subject1marks=int(input('enter subject 1 marks'))
+subject2marks=int(input('enter subject 2 marks'))
+subject3marks=int(input('enter subject 3 marks'))
+                        
+
+totalmarks =subject1marks+subject2marks+subject3marks
+averagemarks=(totalmarks)/3
+print('student Name=',name)
+print('total marks=',totalmarks)
+print('average marks=',averagemarks)
